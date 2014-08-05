dfaaf2a0b4a5157dfeab07ac3c588e6160b5ced7
bjon045 <bruce.jones@suncorp.co.nz>
2014-08-05 14:41:10 +1200

    added example trigger

--- src/bioroid/control/EventController.java
+++ src/bioroid/control/EventController.java
@@ -1,6 +1,16 @@
 package bioroid.control;
 
+import java.util.List;
+
 import org.newdawn.slick.GameContainer;
+
+import bioroid.GameHolder;
+import bioroid.model.ModelResources;
+import bioroid.model.character.GameCharacter;
+import bioroid.model.event.Event;
+import bioroid.model.event.TriggerPoint;
+import bioroid.model.location.GameMap;
+import bioroid.utils.CharacterUtils;
 
 /**
  * Event Controller should determine if any events have been triggered and update the appropriate entities/perform
@@ -9,7 +19,20 @@ import org.newdawn.slick.GameContainer;
 public class EventController {
 
     public void update(GameContainer container) {
-        // TODO
+
+        GameCharacter mainCharacter = CharacterUtils.getMainCharacter();
+        List<Event> events = ModelResources.events.getEvents();
+
+        GameMap gameMap = GameHolder.currentMap;
+        // TODO optimisation: events should be preloaded onto the map
+
+        for (Event event : events) {
+            for (TriggerPoint tp : event.getTriggerPoints()) {
+                if (tp.isTriggered(mainCharacter.getLocation())) {
+                    System.out.println("Event Triggered: " + event.getCode());
+                }
+            }
+        }
     }
 
 }
