82c7f44b5b9697d122f68d11ff683af29b98343c
bjon045 <bruce.jones@suncorp.co.nz>
2014-08-12 12:41:11 +1200

    started on AI

---
+++ src/bioroid/model/character/ai/ArtificalIntelligenceType.java
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2014, Suncorp Metway Limited. All rights reserved.
+//
+// This is unpublished proprietary source code of Suncorp Metway Limited.
+// The copyright notice above does not evidence any actual or intended
+// publication of such source code.
+//
+////////////////////////////////////////////////////////////////////////////////
+// $Id$
+// $Revision$
+// $Date$
+// $Author$
+////////////////////////////////////////////////////////////////////////////////
+package bioroid.model.character.ai;
+
+public enum ArtificalIntelligenceType {
+    STATIONARY, RANDOM, WORKER, HOSTILE
+}
