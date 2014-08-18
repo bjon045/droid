006b4f6c0f4aa7c10467bb395df208bc872c6e97
bjon045 <bruce.jones@suncorp.co.nz>
2014-08-18 13:36:29 +1200

    removed comment

--- src/bioroid/model/character/ai/ArtificalIntelligenceType.java
+++ src/bioroid/model/character/ai/ArtificalIntelligenceType.java
@@ -1,17 +1,3 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-// Copyright (c) 2014, Suncorp Metway Limited. All rights reserved.
-//
-// This is unpublished proprietary source code of Suncorp Metway Limited.
-// The copyright notice above does not evidence any actual or intended
-// publication of such source code.
-//
-////////////////////////////////////////////////////////////////////////////////
-// $Id$
-// $Revision$
-// $Date$
-// $Author$
-////////////////////////////////////////////////////////////////////////////////
 package bioroid.model.character.ai;
 
 public enum ArtificalIntelligenceType {
